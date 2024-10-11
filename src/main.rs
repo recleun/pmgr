0c52afcb31c237e574d4ab8b4ba08b1048bbceba
recleun <98621222+recleun@users.noreply.github.com>
2024-10-12 02:17:11 +0300

    Write main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+use pmgr::commands::{Cli, Commands, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Check(args) => args.run(None),
+        Commands::Read(args) => args.run(None),
+        Commands::Init(args) => args.run(None),
+    }
 }
