170a20eef9daf842f1b424cf2f25a4cea9d487bf
recleun <98621222+recleun@users.noreply.github.com>
2024-10-14 01:33:52 +0300

    Refactor to use HashMap type

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -12,21 +12,22 @@ impl super::Command for CreateArgs {
     fn run(self, file_name: Option<&str>) {
         let mut data: Project = utils::get_data(file_name);
 
-        if let Some(_) = data.get_group(self.group_name.as_str()) {
+        if data.groups.contains_key(&self.group_name) {
             eprintln!("A group with the name `{}` already exists", self.group_name);
             return;
         }
 
         if let Some(parent_name) = self.parent_group {
-            match data.get_group(&parent_name) {
-                Some(parent) => data.groups[parent].groups.push(self.group_name.clone()),
-                None => {
-                    eprintln!("Specified parent group was not found");
-                    return;
-                }
-            };
+            if data.groups.contains_key(&parent_name) {
+                let mut parent = data.get_group(&parent_name).unwrap().clone();
+                parent.groups.push(self.group_name.clone());
+                data.groups.insert(parent_name, parent);
+            } else {
+                eprintln!("Specified parent group was not found");
+                return;
+            }
         }
-        data.groups.push(Group::new(&self.group_name));
+        data.groups.insert(self.group_name.clone(), Group::new(&self.group_name));
         utils::write_data(file_name, &data);
 
         println!("Added group `{}` to project successfully", self.group_name);
