ca4c533072102c504837e9185e33c60ad14654a0
recleun <98621222+recleun@users.noreply.github.com>
2024-10-23 03:40:27 +0300

    Improve code readability for no info display check

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -12,20 +12,22 @@ impl super::Command for InfoArgs {
 
         println!();
 
+        let mut info_displayed = false;
+
         if let Some(title) = &data.information.title {
             println!("Project Title: {}", title);
+            info_displayed = true;
         }
         if let Some(desc) = &data.information.description {
             println!("Description: {}", desc);
+            info_displayed = true;
         }
         if let Some(repo) = &data.information.repo {
             println!("Project Title: {}", repo);
+            info_displayed = true;
         }
 
-        if data.information.title.is_none()
-            && data.information.description.is_none()
-            && data.information.repo.is_none()
-        {
+        if !info_displayed {
             println!("No project information was set, use `pmgr set` command to add information");
         }
     }
