d064075f91cbb6183cadf29bce0776d4a680d310
recleun <98621222+recleun@users.noreply.github.com>
2024-10-25 15:17:21 +0300

    Simplify tests, put helpers in separate file

---
+++ tests/common/mod.rs
@@ -0,0 +1,105 @@
+use std::fs;
+
+/// Creates a list of groups for a test
+/// ```rs
+/// create_groups(
+///     file_name,
+///     // group1 will be created
+///     "group1" -> []
+///     // group2 will be created with group3 and group4 as subgroups
+///     "group2" -> ["group3", "group4"]
+///     // group4 will be created with group5 as a subgroup
+///     "group4" -> ["group5"]
+/// );
+/// ```
+#[macro_export]
+macro_rules! create_groups {
+    (
+        $file_name:ident,
+        $($group:literal -> [$($subgroup:literal$(,)?)*],)*
+    ) => {
+        $(
+            commands::create::CreateArgs {
+                group_name: $group.to_string(),
+                parent_group: None,
+            }.run($file_name);
+            $(
+                commands::create::CreateArgs {
+                    group_name: $subgroup.to_string(),
+                    parent_group: Some($group.to_string()),
+                }.run($file_name);
+            )*
+        )*
+    };
+}
+
+/// Inserts a list of groups into a project
+/// ```rs
+/// // Insert group objects
+/// insert_groups!(project, group1, group2, group3);
+///
+/// // Or create them on demand
+/// insert_groups!(project, "group1", "group2", "group3");
+/// ```
+#[macro_export]
+macro_rules! insert_groups {
+    (
+        $project:ident, $($group:ident$(,)?)*
+    ) => {
+        $($project.groups.insert($group.name.to_string(), $group);)*
+    };
+    (
+        $project:ident, $($group:literal$(,)?)*
+    ) => {
+        $($project.groups.insert($group.to_string(), Group::new($group));)*
+    };
+}
+
+/// Push a list of groups into a group
+/// ```rs
+/// push_groups!(group, "subgroup1", "subgroup2");
+/// ```
+#[macro_export]
+macro_rules! push_groups {
+    (
+        $group:ident, $($subgroup:literal$(,)?)*
+    ) =>{
+        $($group.groups.push($subgroup.to_string());)*
+    };
+}
+
+/// Set watch status for a list of groups
+/// ```rs
+/// // To watch:
+/// watch_groups!(file_name, true, "group1", "group2");
+/// // To unwatch:
+/// watch_groups!(file_name, false, "group1", "group2");
+/// ```
+#[macro_export]
+macro_rules! watch_groups {
+    (
+        $file_name:ident, true, $($group:literal$(,)?)*
+    ) =>{
+        $(commands::watch::WatchArgs {
+            group_names: vec![$group.to_string()]
+        }.run($file_name);)*
+    };
+    (
+        $file_name:ident, false, $($group:literal$(,)?)*
+    ) =>{
+        $(commands::unwatch::UnwatchArgs {
+            group_names: vec![$group.to_string()],
+            all: false,
+        }.run($file_name);)*
+    };
+}
+
+/// Ensures that the specified file_name does not exist, and if it does, gets removed.
+pub fn clean(file_name: &str) {
+    let file = fs::metadata(file_name).is_ok();
+    if file {
+        if let Err(e) = fs::remove_file(file_name) {
+            panic!("{}", e);
+        }
+    }
+}
--- tests/core_features.rs
+++ tests/core_features.rs
@@ -1,334 +1,175 @@
-use std::fs;
-
-/// Set watch status for a list of groups
-/// ```rs
-/// // To watch:
-/// watch_groups!(file_name, true, "group1", "group2");
-/// // To unwatch:
-/// watch_groups!(file_name, false, "group1", "group2");
-/// ```
-macro_rules! watch_groups {
-    (
-        $file_name:ident, true, $($group:literal$(,)?)*
-    ) =>{
-        $(commands::watch::WatchArgs {
-            group_names: vec![$group.to_string()]
-        }.run($file_name);)*
-    };
-    (
-        $file_name:ident, false, $($group:literal$(,)?)*
-    ) =>{
-        $(commands::unwatch::UnwatchArgs {
-            group_names: vec![$group.to_string()],
-            all: false,
-        }.run($file_name);)*
-    };
-}
-
-/// Simply checks whether or not the project is even initalized.
-macro_rules! generate_ending_check1 {
-    ($file_name: ident, $_: expr) => {
-        if !fs::metadata($file_name).is_ok() {
-            panic!("Project was not initalized");
-        }
-    };
-}
-
-/// This checks the current saved data to that of "project" (the second passed item).
-macro_rules! generate_ending_check2 {
-    ($file_name: ident, $project: expr) => {{
-        let Some(data) = utils::get_data($file_name) else {
-            panic!("Failed to get data");
-        };
-        assert_eq!(data, $project);
-    }};
-}
-
-/// This just checks if the file does _not_ exist, and if not, checks if the error
-/// is [`NotFound`](std::io::ErrorKind::NotFound).
-macro_rules! generate_ending_check3 {
-    ($file_name: ident, $_: expr) => {{
-        if let Err(e) = utils::check_data($file_name) {
-            if e.kind() != io::ErrorKind::NotFound {
-                panic!("{}", e);
-            }
-        }
-    }};
-}
-
-/// Generates ending checks for watch/unwatch commands.
-///
-/// ```rs
-/// generate_watch_ending_check!(file_name, {
-///     [<groups-to-edit>] -> <watch> => [ <final-groups> ]
-/// })
-/// ```
-///
-/// `<groups-to-edit>` are the groups that you want to either watch or unwatch, and
-/// `<watch>` is a boolean indicating whether to watch or not. The final part is
-/// optional, when present, it'll check the current values of watched tables and make
-/// sure it matches `<final-groups>` (or panics if not).
-macro_rules! generate_watch_ending_check {
-    (
-        $file_name: ident,
-        {
-           $( [ $($name: literal),* $(,)? ] -> $value: literal $(=> [ $($match: literal),* $(,)?] )? ),* $(,)?
-        }
-    ) => {{
-        $(
-            if $value {
-                watch_groups!($file_name, true $(, $name)*);
-            } else  {
-                watch_groups!($file_name, false $(, $name)*);
-            }
-            $(
-                let Some(data) = utils::get_data($file_name) else {
-                    panic!("Failed to get data");
-                };
-                let matches: Vec<&str> = vec![$($match,)*];
-                assert_eq!(data.active_groups, matches);
-            )?
-        )*
-    }};
-}
-
-/// Internal macro for `generate_test`.
-macro_rules! __generate_test {
-    (
-        $file_name_variable: ident = $file_name: literal,
-        $project: ident
-        $(, $extra_block: block)?
-        $(, $generate_extra_block_after: ident)?
-        {
-            $( $group: ident -> [
-                $( $subgroup: ident$(,)?)*
-            ], )*
-        }
-        $(, $ending_check: block)?
-    ) => {
-        let $file_name_variable = $file_name;
-        clean($file_name_variable);
-
-        // just here
-        #[allow(unused_mut, unused_variables)]
-        let mut $project = Project::new();
-
-        $(
-        #[allow(unused_mut, unused_variables)]
-            let mut $group = Group::new(stringify!($group));
-            $(
-                let $subgroup = Group::new(stringify!($subgroup));
-                $group.groups.push(stringify!($subgroup).to_string());
-                $project.groups.insert(stringify!($subgroup).to_string(), $subgroup);
-            )*
-
-            $project.groups.insert(stringify!($group).to_string(), $group);
-        )*
-
-        $($extra_block)?
-
-        commands::init::InitArgs.run($file_name_variable);
-
-        $( $generate_extra_block_after!($file_name_variable); )?
-
-        $(
-            commands::create::CreateArgs {
-                group_name: stringify!($group).to_string(),
-                parent_group: None,
-            }.run($file_name_variable);
-            $(
-                commands::create::CreateArgs {
-                    group_name: stringify!($subgroup).to_string(),
-                    parent_group: Some(stringify!($group).to_string()),
-                }.run($file_name_variable);
-            )*
-        )*
-
-        $( $ending_check )?
-        clean($file_name_variable);
-    };
-}
-
-/// The actual macro to generate tests. The simplest form is:
-///
-/// ```rs
-/// generate_test!(".test.pmgr" => {}, generate_ending_check1)
-/// ```
-///
-/// Which simply creates a new pmgr project and checks whether or not it exists.
-///
-/// The macro has 2 match arms, the first 2 accepted values are the same in both, and
-/// they are:
-/// *  file_name - the file name that the test will be applied on. Should be different
-///    for every test to ensure they don't overlap!
-/// * extra_block - an optional block of code which is ran before
-///   [`Init`](pmgr::commands::init::InitArgs) command is ran.
-///
-/// After those 2, the 3rd value varies, in match arm #1, it's a simple `ident`, which
-/// must be a macro that accepts an `ident` and an `expr`. That macro is called at the
-/// very end of the test, right before the final [`clean`].
-///
-/// For match arm #2, the syntax goes like:
-/// ```rs
-/// (file_name, project) => { ... }
-/// ```
-///
-/// Where `file_name` and `project` are an `ident` and an `expr` respectfully. What's
-/// in the body (`{ ... }`) is completely up to you which gives you complete freedom
-/// of what to do inside it.
-macro_rules! generate_test {
-    (
-        $file_name: literal => {
-            $( $group: ident -> [
-                $( $subgroup: ident$(,)?)*
-            ], )*
-        }
-        $(, $extra_block: block )?
-        $(, $generate_ending_check: ident )?
-    ) => {
-        __generate_test!(
-            file_name_variable = $file_name,
-            project
-            $(, $extra_block )?
-            {
-                $( $group -> [$( $subgroup, )*], )*
-            },
-            { $( $generate_ending_check!(file_name_variable, project) )? }
-        )
-    };
-    (
-        $file_name: literal => {
-            $( $group: ident -> [
-                $( $subgroup: ident$(,)?)*
-            ], )*
-        }
-        $(, $extra_block: block )?
-        $( ($file_name_variable: ident, $project: ident) => $body: block )?
-    ) => {
-        __generate_test!(
-            file_name_variable = $file_name,
-            project
-            $(, $extra_block )?
-            {
-                $( $group -> [$( $subgroup, )*], )*
-            },
-            {
-                $(
-                    // to avoid annoying warnings.
-                    #[allow(unused_variables)]
-                    let $file_name_variable = file_name_variable;
-                    #[allow(unused_variables)]
-                    let $project = project;
-                    $body;
-                )?
-            }
-        )
-    };
-}
-
-fn clean(file_name: &str) {
-    let file = fs::metadata(file_name).is_ok();
-    if file {
-        if let Err(e) = fs::remove_file(file_name) {
-            panic!("{}", e);
-        }
-    }
-}
+mod common;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use core::panic;
-    use pmgr::{
-        commands,
-        data::{Group, Project},
-        utils, Command,
-    };
     use std::{fs, io};
+    use pmgr::{
+        commands, data::{Group, Project}, utils, Command
+    };
+    use common;
 
     #[test]
     fn simple_init() {
-        generate_test!(".simple-init.pmgr" => {}, generate_ending_check1);
+        let file_name = ".simple-init.pmgr";
+        common::clean(file_name);
+
+        commands::init::InitArgs.run(file_name);
+
+        if !fs::metadata(file_name).is_ok() {
+            panic!("Project was not initalized");
+        }
+        common::clean(file_name);
     }
 
     #[test]
     fn simple_check() {
         let file_name = ".simple-check.pmgr";
-        generate_test!(
-            ".simple-check.pmgr" => {},
-            {
-                if let Ok(data) = utils::check_data(file_name) {
-                    panic!("Data shouldn't exist, exists at: {:?}", data)
+        common::clean(file_name);
+
+        // data doesn't exist for this check
+        match utils::check_data(file_name) {
+            Ok(data) => panic!("Data shouldn't exist, exists at: {:?}", data),
+            Err(_) => (),
+        }
+
+        commands::init::InitArgs.run(file_name);
+
+        // data exists for this check
+        match utils::check_data(file_name) {
+            Ok(_) => (),
+            Err(e) => {
+                if e.kind() != io::ErrorKind::NotFound {
+                    panic!("{}", e);
                 }
-            },
-            generate_ending_check3
-        );
+            }
+        }
+        common::clean(file_name);
     }
 
     #[test]
     fn simple_create() {
-        generate_test!(
-            ".simple-create.pmgr" => {
-                group1 -> [],
-                group2 -> [],
-                group3 -> [],
-                group3 -> [],
-            },
-            generate_ending_check2
+        let file_name = ".simple-create.pmgr";
+        common::clean(file_name);
+
+        let mut project = Project::new();
+
+        insert_groups!(project, "group1", "group2", "group3");
+
+        commands::init::InitArgs.run(file_name);
+
+        create_groups!(
+            file_name,
+            "group1" -> [],
+            "group2" -> [],
+            "group3" -> [],
+            // should not create duplicates
+            "group3" -> [],
         );
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data, project);
+
+        common::clean(file_name);
     }
 
     #[test]
     fn create_with_parent() {
-        generate_test!(
-            ".create-with-parent.pmgr" => {
-                group1 -> [],
-                group1 -> [],
-                group2 -> [group3, group4],
-                group4 -> [group5],
-            },
-            generate_ending_check2
+        let file_name = ".create-with-parent.pmgr";
+        common::clean(file_name);
+
+        let mut project = Project::new();
+        let group1 = Group::new("group1");
+        let mut group2 = Group::new("group2");
+        let group3 = Group::new("group3");
+        let mut group4 = Group::new("group4");
+        let group5 = Group::new("group5");
+
+        push_groups!(group2, "group3", "group4");
+        push_groups!(group4, "group5");
+
+        insert_groups!(project, group1, group2, group3, group4, group5);
+
+        commands::init::InitArgs.run(file_name);
+
+        create_groups!(
+            file_name,
+            "group1" -> [],
+            "group2" -> ["group3", "group4"],
+            "group4" -> ["group5"],
         );
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data, project);
+
+        common::clean(file_name);
     }
 
     #[test]
-    fn simple_watch() {
-        generate_test!(
-            ".simple-select.pmgr" => {
-                group1 -> [],
-                group2 -> [group3, group4],
-                group4 -> [group5],
-            }
-            (file_name, project) => {
-                generate_watch_ending_check!(
-                    file_name,
-                    {
-                        ["group4"] -> true => ["group4", "group5"],
-                        ["group1", "group2"] -> true => ["group1", "group2", "group3", "group4", "group5"],
-                    }
-                )
-            }
+    fn simple_select() {
+        let file_name = ".simple-select.pmgr";
+        common::clean(file_name);
+
+        commands::init::InitArgs.run(file_name);
+
+        create_groups!(
+            file_name,
+            "group1" -> [],
+            "group2" -> ["group3", "group4"],
+            "group4" -> ["group5"],
         );
+
+        watch_groups!(file_name, true, "group4");
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data.active_groups, vec!["group4", "group5"]);
+
+        watch_groups!(file_name, true, "group1", "group2");
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data.active_groups, vec!["group1", "group2", "group3", "group4", "group5"]);
+
+        common::clean(file_name);
     }
 
     #[test]
-    fn simple_unwatch() {
-        generate_test!(
-            ".simple-deselect.pmgr" => {
-                group1 -> [],
-                group2 -> [group3, group4],
-                group4 -> [group5],
-            }
-            (file_name, project) => {
-                generate_watch_ending_check!(
-                    file_name,
-                    {
-                        ["group2"] -> true,
-                        ["group5"] -> false => ["group2", "group3", "group4"],
-                        ["group2"] -> false => [],
-                    }
-                )
-            }
+    fn simple_deselect() {
+        let file_name = ".simple-deselect.pmgr";
+        common::clean(file_name);
+
+        commands::init::InitArgs.run(file_name);
+
+        create_groups!(
+            file_name,
+            "group1" -> [],
+            "group2" -> ["group3", "group4"],
+            "group4" -> ["group5"],
         );
+
+        watch_groups!(file_name, true, "group2");
+        watch_groups!(file_name, false, "group5");
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data.active_groups, vec!["group2", "group3", "group4"]);
+
+        watch_groups!(file_name, false, "group2");
+
+        let Some(data) = utils::get_data(file_name) else {
+            panic!("Failed to get data");
+        };
+        assert_eq!(data.active_groups.len(), 0);
+
+        common::clean(file_name);
     }
 }
